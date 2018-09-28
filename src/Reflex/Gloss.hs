ec11dd09a7bd3f6c8f2c72f7b7c19900aadb44ae
Isaac Elliott <isaace71295@gmail.com>
2018-09-28 21:10:58 +1000

    Implemented graceful shutdown

--- src/Reflex/Gloss.hs
+++ src/Reflex/Gloss.hs
@@ -17,66 +17,72 @@
 module Reflex.Gloss
   ( playReflex
   , InputEvent
-  , GlossApp )
-  where
+  , GlossApp
+  )
+where
 
-import           Control.Concurrent (forkIO)
-import           Control.Concurrent.STM (atomically)
-import           Control.Concurrent.STM.TVar (newTVarIO, writeTVar, readTVarIO)
-import           Control.Monad (void)
-import           Control.Monad.Fix      (MonadFix)
-import           Control.Monad.Identity
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Dependent.Sum (DSum ((:=>)))
-import           Data.IORef             (readIORef)
+import Reflex
+import Reflex.Host.Basic
 
+import Control.Concurrent (forkFinally)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TVar (newTVarIO, writeTVar, readTVarIO)
+import Control.Monad (void)
+import Control.Monad.IO.Class (liftIO)
+import Graphics.Gloss (Color, Display, Picture, blank)
+import Graphics.Gloss.Interface.IO.Game (playIO)
+import System.Exit (exitSuccess)
 
-import           Graphics.Gloss
-  (Color, Display, Picture, blank)
-import           Graphics.Gloss.Interface.IO.Game (playIO)
 import qualified Graphics.Gloss.Interface.IO.Game as G
-
-import           Reflex
-import           Reflex.Host.Class
-  (newEventWithTriggerRef, runHostFrame, fireEvents, fireEventRef)
-import           Reflex.Host.Basic
 
 -- | Synonym for a Gloss Event to prevent name clashes.
 type InputEvent = G.Event
 
--- | Convert the refresh and input events to a Behavior t Picture.
+-- | Convert the refresh and input events to a @'Dynamic' 'Picture'@
 type GlossApp t m
   = BasicGuestConstraints t m
   => Event t Float
   -> Event t InputEvent
-  -> BasicGuest t m (Dynamic t Picture)
--- | Play the 'GlossApp' in a window, updating when the Behavior t Picture
---   changes.
+  -> BasicGuest t m (Dynamic t Picture, Event t ())
+
+-- | Play the 'GlossApp' in a window
 playReflex
-  :: Display                    -- ^ Display mode.
-  -> Color                      -- ^ Background color.
-  -> Int                        -- ^ Maximum frames per second.
+  :: Display -- ^ Display mode.
+  -> Color -- ^ Background color.
+  -> Int -- ^ Maximum frames per second.
   -> (forall t m. GlossApp t m) -- ^ A reflex function that returns a 'Behavior t Picture'
   -> IO ()
 playReflex display color frequency network =
-  basicHostForever $ do
-    picMVar <- liftIO $ newTVarIO blank
-    timeMVar <- liftIO $ newTVarIO 0.0
+  basicHostWithQuit $ do
+    picTVar <- liftIO $ newTVarIO blank
+    quitTVar <- liftIO $ newTVarIO False
 
     (tickEvent, tickTrigger)  <- newTriggerEvent
     (inputEvent, inputTrigger) <- newTriggerEvent
+    (hostQuitEvent, hostQuitTrigger) <- newTriggerEvent
 
-    dPicture <- network tickEvent inputEvent
+    (dPicture, eQuit) <- network tickEvent inputEvent
 
     performEvent_ $
-      liftIO . atomically . writeTVar picMVar <$>
+      liftIO . atomically . writeTVar picTVar <$>
       updated dPicture
 
-    liftIO . void . forkIO $
-      playIO display
-      color
-      frequency
-      ()
-      (\_ -> readTVarIO picMVar)
-      (\ge _ -> inputTrigger ge)
-      (\fl _ -> tickTrigger fl)
+    performEvent_ $
+      (liftIO . atomically $ writeTVar quitTVar True) <$ eQuit
+
+    void . liftIO .
+      flip forkFinally (\_ -> hostQuitTrigger ()) $
+      playIO
+        display
+        color
+        frequency
+        ()
+        (\_ -> readTVarIO picTVar)
+        (\ge _ -> inputTrigger ge)
+        (\fl _ -> do
+            shouldQuit <- readTVarIO quitTVar
+            if shouldQuit
+              then exitSuccess
+              else tickTrigger fl)
+
+    pure ((), hostQuitEvent)
